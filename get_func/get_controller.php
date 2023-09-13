a0653c89b1d0b61e32b6f001933d4bce0ada95d7
Artem <helltema@mail.ru>
2023-09-13 21:31:23 +0400

    feat: add comments

--- get_func/get_controller.php
+++ get_func/get_controller.php
@@ -1,12 +1,18 @@
 <?php
 
+//Запрос пользователей
 function getUsers($connect) {
+    // кодирование в json > сбор ответа в ассоциативный масив > запрос в бд
     echo json_encode(mysqli_fetch_assoc(mysqli_query($connect, "SELECT * FROM `users`")));
 }
 
+//Запрос пользователя
 function getUser($connect, $id)
 {
+    // запрос в бд
     $response = mysqli_query($connect, "SELECT * FROM `users` WHERE `id` = $id");
+
+    // проверка на количество колонн ответа
     if(mysqli_num_rows($response) === 0)
     {
         http_response_code(404);
@@ -14,6 +20,7 @@ function getUser($connect, $id)
         echo json_encode(["error" => true, "message" => "Не удалось найти пользователя"]);
     }else
     {
+        // кодирование в json > сбор ответа в ассоциативный масив
         echo json_encode(mysqli_fetch_assoc($response));
     }
 }
