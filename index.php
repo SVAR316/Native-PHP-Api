32e716de782384d6336b0974906bee10de54c68c
Artem <helltema@mail.ru>
2023-09-15 14:24:59 +0400

    feat: new post method
    - new post method
    - new valid func
    - new create class
    - new checks request method

--- index.php
+++ index.php
@@ -1,28 +1,64 @@
 <?php
-
 // Задаем в header тип данных json
 header("Content-type: json/application");
 
 //Подключение файлов
 $connect = require 'db.php';
-$get_controller = require 'get_func/get_controller.php';
+$get_controller_file = require 'get_func/get_controller.php';
+$post_controller_file = require 'post_func/post_controller.php';
+//Создание экземпляра класса
+$get_controller = new getController();
+$post_controller = new postController();
+
 
 
 // получение параметра после http://example/
 $request = $_GET['q'];
+// Разбиваем запрос
 $params = explode('/', $request);
 $query = $params[0];
+//Получаем метод запроса
+$method = $_SERVER['REQUEST_METHOD'];
+// Получаем тип контента
+$content_type = $_SERVER['CONTENT_TYPE'];
 
-if($query === "users"){
-    if(isset($params[1]))
-    {
-        $get_controller->getUser($connect, $params[1]);
-    }else
-    {
-        $get_controller->getUsers($connect);
+
+
+// Проверяем тип запроса
+if ($method === 'GET') {
+    switch ($query) {
+        case "users":
+            // проверка на доп параметры в запросе
+            if (isset($params[1])) {
+                $get_controller->getUser($connect, $params[1]);
+            } else {
+                $get_controller->getUsers($connect);
+            }
+            break;
+        default:
+            http_response_code(404);
+            echo json_encode(["error" => true]);
+            break;
     }
-}else {
+}
+elseif($method === 'POST')
+{
+    switch ($query)
+    {
+        case "users":
+            $post_controller->createUser($connect, file_get_contents('php://input'), $content_type);
+            break;
+
+        default:
+            http_response_code(404);
+            echo json_encode(["error" => true]);
+            break;
+    }
+}
+else
+{
     http_response_code(404);
     echo json_encode(["error" => true]);
 }
+
 
--- post_func/post_controller.php
+++ post_func/post_controller.php
@@ -1,5 +1,34 @@
 <?php
 
-class PostController {
+class postController
+{
+    // $connect = подключение к бд, $data = данные пользователя, $content_type = тип данных
+    function createUser(mysqli $connect, $json_data, $content_type): void
+    {
+        if ($content_type === 'application/json') {
+            if ($this->validationUserData($json_data)) {
+                $data = json_decode($json_data, true);
+                $user_name = $data["name"];
+                $connect->query("INSERT INTO `users` (`name`) VALUES (\"{$user_name}\")");
+                http_response_code(201);
+                echo json_encode(["error" => 'false', 'post_id' => mysqli_insert_id($connect)]);
+            } else {
+                http_response_code(400);
+                echo json_encode(["error" => 'true']);
+            }
+        } else {
+            http_response_code(400);
+            echo json_encode(["error" => 'true']);
+        }
 
+    }
+
+    function validationUserData($json_data): bool
+    {
+        $data = json_decode($json_data, true);
+        if (empty($data['name'])) {
+            return false;
+        }
+        return true;
+    }
 }
