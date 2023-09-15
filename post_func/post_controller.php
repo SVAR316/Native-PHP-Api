1c7f21b0eb513ad54c38ee77adc3bd4680123e5e
Artem <helltema@mail.ru>
2023-09-15 14:26:30 +0400

    feat: add comment

--- post_func/post_controller.php
+++ post_func/post_controller.php
@@ -5,10 +5,14 @@ class postController
     // $connect = подключение к бд, $data = данные пользователя, $content_type = тип данных
     function createUser(mysqli $connect, $json_data, $content_type): void
     {
+        // проверка на тип данных
         if ($content_type === 'application/json') {
+            // валидация
             if ($this->validationUserData($json_data)) {
+                // декодируем json
                 $data = json_decode($json_data, true);
                 $user_name = $data["name"];
+                // делаем запрос к бд
                 $connect->query("INSERT INTO `users` (`name`) VALUES (\"{$user_name}\")");
                 http_response_code(201);
                 echo json_encode(["error" => 'false', 'post_id' => mysqli_insert_id($connect)]);
